79e4f544ddc2687a1a55a5dda974ecc2f8bf032c
785putt <puttizant785@gmail.com>
2025-09-12 14:56:13 +0700

    fix frontend project structure git issues

---
+++ frontend/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+nextConfig.outputFileTracingRoot = '/c:/Users/mrput/Documents/VSProject/MyFullstackApp';
